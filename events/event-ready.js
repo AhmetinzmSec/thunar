dc0ee2b629794a1199c5603936001d1fa007c825
AhmetinzmSec <98494934+AhmetinzmSec@users.noreply.github.com>
2022-08-26 12:19:33 +0300

    Update event-ready.js

--- events/event-ready.js
+++ events/event-ready.js
@@ -17,8 +17,8 @@ module.exports = client => {
 
         `${version} || !şartlar`,
         `${version} || !yardım`,
-        `${version} || Efsanevi Güncellemeye Son 6 Gün!`,
-        `${version} || Büyük Değişime 6 Gün!`,
+        `${version} || Efsanevi Güncellemeye Son 5 Gün!`,
+        `${version} || Büyük Değişime 5 Gün!`,
         `${version} || Mulan Hazır! Çok Yakında Sizlerle...`,
         `${version} || ${version_name}`
 
