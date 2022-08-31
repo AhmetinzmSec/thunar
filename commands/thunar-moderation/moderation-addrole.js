5d7b0bec09fd0fd5fe59b8877fd551b850b04e79
AhmetinzmSec <98494934+AhmetinzmSec@users.noreply.github.com>
2022-09-01 00:02:55 +0300

    Add files via upload

--- commands/thunar-moderation/moderation-addrole.js
+++ commands/thunar-moderation/moderation-addrole.js
@@ -17,7 +17,7 @@ exports.run = (client, message, args) => {
     if (!role) return message.reply('Lütfen Vermek İstediğiniz Rolü Etiketleyin!')
     if (!member) return message.reply('Lütfen Rol Vermek İstediğiniz Kişiyi Etiketleyin!')
     member.roles.add(role)
-    
+
     const embed = new MessageEmbed()
         .setTitle(`Roller Değiştirildi`)
         .setDescription(`**Rol Verilen Kullanıcı: **${message.mentions.users.first()}\n**Verilen Rol: **${role}\n**Yetkili: <@${message.author.id}>**`)
